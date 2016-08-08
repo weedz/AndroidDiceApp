89bbbcc6dcc84404d4b97cd359ba9d5d9ad94caf
WeeDz <weedz@users.noreply.github.com>
2016-08-08 19:03:59 +0200

    Threads are good, yes? UI improvements

--- app/src/main/java/com/weedz/dice/Data.java
+++ app/src/main/java/com/weedz/dice/Data.java
@@ -2,10 +2,11 @@ package com.weedz.dice;
 
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Observable;
 
 /**
@@ -19,25 +20,44 @@ public class Data extends Observable {
         return ourInstance;
     }
 
-    private ArrayList<Integer> dice;
+    // Flag values
+    public static final int FLAG_DIEBAG_UPDATE = 0;
+    public static final int FLAG_DICE_ROLL = 1;
+    public static final int FLAG_THREAD_LOCK = 2;
+    // flags
+    private Boolean[] flags = new Boolean[3];
+
+    private volatile ArrayList<Integer> dice;
+    private int total = 0;
+    private int largestDie = 6;
 
     private RollThread mRollThread;
-    private RollHandler handler = new RollHandler();
+    WeakReference<Data> ref = new WeakReference<>(this);
+    private RollHandler handler = new RollHandler(ref);
 
     private Data() {
+        Arrays.fill(flags, false);
         dice = new ArrayList<>();
         dice.add(0);
     }
 
-    public void setUpdate() {
+    private void setUpdate() {
         setChanged();
         notifyObservers();
     }
 
-    public void addDie(int nr) {
-        /*for (int i = 0; i < nr; i++) {
-            dice.add(0);
-        }*/
+    public boolean getFlag(int flag) {
+        return flags[flag];
+    }
+    public void setFlag(int flag, Boolean b) {
+        flags[flag] = b;
+    }
+
+    public synchronized void addDice(int nr, int sides) {
+        if (flags[FLAG_THREAD_LOCK]) {
+            Log.d(TAG,"addDice(): ThreadLock");
+            return;
+        }
         if (mRollThread != null && mRollThread.isAlive()) {
             mRollThread.interrupt();
         }
@@ -45,9 +65,15 @@ public class Data extends Observable {
             nr = 1000-dice.size();
         }
         dice.addAll(Arrays.asList(new Integer[nr]));
+        setFlag(FLAG_DIEBAG_UPDATE, true);
         setUpdate();
     }
-    public void removeDie(int nr) {
+
+    /*public synchronized void removeDie(int nr) {
+        if (flags[FLAG_THREAD_LOCK]) {
+            Log.d(TAG,"removeDie(): ThreadLock");
+            return;
+        }
         if (mRollThread != null && mRollThread.isAlive()) {
             mRollThread.interrupt();
         }
@@ -58,64 +84,92 @@ public class Data extends Observable {
                 break;
             }
         }
+        setFlag(FLAG_DIEBAG_UPDATE, true);
+        setUpdate();
+    }*/
+    public synchronized void removeDie(int nr, int sides) {
+        if (flags[FLAG_THREAD_LOCK]) {
+            Log.d(TAG,"removeDie(): ThreadLock");
+            return;
+        }
+        if (mRollThread != null && mRollThread.isAlive()) {
+            mRollThread.interrupt();
+        }
+        for (int i = 0; i < nr; i++) {
+            if (dice.size() > 0) {
+                dice.remove(0);
+            } else {
+                break;
+            }
+        }
+        setFlag(FLAG_DIEBAG_UPDATE, true);
         setUpdate();
     }
-    public void setDice(int nr) {
+
+    public synchronized void setDice(int nr, int sides) {
+        largestDie = sides;
+        if (flags[FLAG_THREAD_LOCK]) {
+            Log.d(TAG,"setDice(): ThreadLock");
+            return;
+        }
         if (mRollThread != null && mRollThread.isAlive()) {
             mRollThread.interrupt();
         }
         dice.clear();
-        addDie(nr);
-        setUpdate();
+        addDice(nr, sides);
     }
 
-    // TODO: run in a separate thread?
-    public void roll() {
-        /*if (mRollThread != null && !mRollThread.isAlive()) {
-            mRollThread.interrupt();
-            mRollThread = null;
-        }
-        if (mRollThread == null) {
-            mRollThread = new RollThread();
-            mRollThread.start();
-        }*/
-        for(int i=0; i < dice.size(); i++) {
-            dice.set(i, (int) (Math.random()*6+1));
-        }
-        setUpdate();
-    }
     public int getTotal() {
-        if (mRollThread != null && mRollThread.isAlive()) {
-            return 0;
-        }
-        int total = 0;
-        for(int i=0; i < dice.size(); i++) {
-            total += dice.get(i);
-        }
         return total;
     }
-    public int getDie(int i) {
+
+    public synchronized int getDie(int i) {
         if (mRollThread != null && mRollThread.isAlive()) {
+            Log.d(TAG, "getDie(): interrupt");
+            mRollThread.interrupt();
+        }
+        if (dice.size() < i || dice.get(i) == null) {
+            Log.d(TAG, "getDie(" + i + "): OutOfBounds");
             return 0;
         }
-        if (dice.size() >= i) {
-            return dice.get(i);
-        }
-        return 0;
+        return dice.get(i);
     }
 
     public int getNrOfDie() {
         return dice.size();
     }
 
+    // TODO: run in a separate thread?
+    public synchronized void roll() {
+        if (mRollThread != null) {
+            mRollThread.interrupt();
+            mRollThread = null;
+        }
+        mRollThread = new RollThread();
+        mRollThread.start();
+    }
+
+    public int getLargestDie() {
+        return largestDie;
+    }
+
     static class RollHandler extends Handler {
+        WeakReference<Data> ref;
+        public RollHandler(WeakReference<Data> ref){
+            this.ref = ref;
+        }
+
         @Override
         public void handleMessage(Message msg) {
+            if(msg == null) {
+                Log.d(TAG, "RollHandler() msg == null");
+                return;
+            }
             if (!Thread.currentThread().isInterrupted()) {
-                //Log.print(msg.what);
                 switch (msg.what) {
                     case 1: // Roll finished
-                        Data.getInstance().setUpdate();
+                        ref.get().setFlag(FLAG_DICE_ROLL, true);
+                        ref.get().setUpdate();
                         break;
                 }
             }
@@ -124,12 +178,20 @@ public class Data extends Observable {
     }
 
     private class RollThread extends Thread {
+
         public void run() {
-            for(int i=0; i < dice.size(); i++) {
-                dice.set(i, (int) (Math.random()*6+1));
+            int roll;
+            total = 0;
+            for(int i = 0; i < dice.size(); i++) {
+                roll = (int) (Math.random() * Data.getInstance().getLargestDie() + 1);
+                total += roll;
+                dice.set(i, roll);
             }
-            Log.print(Data.getInstance().dice.get(0));
-            handler.obtainMessage(1).sendToTarget();
+            handler.sendEmptyMessage(1);
+            synchronized (Data.this) {
+                mRollThread = null;
+            }
         }
+
     }
 }
--- app/src/main/java/com/weedz/dice/MainActivity.java
+++ app/src/main/java/com/weedz/dice/MainActivity.java
@@ -1,21 +1,32 @@
 package com.weedz.dice;
 
-import android.app.Application;
 import android.graphics.Typeface;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.ref.WeakReference;
 import java.util.Observable;
 import java.util.Observer;
 
 public class MainActivity extends AppCompatActivity implements Observer {
 
     private static final String TAG = "HomeActivity";
+
+    // Threading stuff
+    private RollUpdateUIThread mRollUpdateThread;
+    private SummaryUpdateUIThread mSummaryUpdateThread;
+    WeakReference<MainActivity> ref = new WeakReference<>(this);
+    RollUpdateUIHandler handler = new RollUpdateUIHandler(ref);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,110 +37,267 @@ public class MainActivity extends AppCompatActivity implements Observer {
         bt_add_die.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (mRollUpdateThread != null) {
+                    mRollUpdateThread.interrupt();
+                    mRollUpdateThread = null;
+                }
+                if (mSummaryUpdateThread != null) {
+                    mSummaryUpdateThread.interrupt();
+                    mSummaryUpdateThread = null;
+                }
                 EditText add_die_nr = (EditText)findViewById(R.id.add_die_nr);
-                int nr = Integer.parseInt(add_die_nr.getText().toString());
-                Data.getInstance().addDie(nr);
+                EditText add_die_sides = (EditText)findViewById(R.id.add_die_sides);
+                try {
+                    int nr = Integer.parseInt(add_die_nr.getText().toString());
+                    int sides = Integer.parseInt(add_die_sides.getText().toString());
+                    if (nr > 0 && sides > 1) {
+                        Data.getInstance().addDice(nr, sides);
+                    }
+                } catch (NumberFormatException e) {
+                    Log.i(TAG, "AddDie():NumberFormatException");
+                }
             }
         });
         Button bt_remove_die = (Button)findViewById(R.id.remove_die_button);
         bt_remove_die.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText remove_die_nr = (EditText)findViewById(R.id.add_die_nr);
-                int nr = Integer.parseInt(remove_die_nr.getText().toString());
-                Data.getInstance().removeDie(nr);
+                if (mRollUpdateThread != null) {
+                    mRollUpdateThread.interrupt();
+                    mRollUpdateThread = null;
+                }
+                if (mSummaryUpdateThread != null) {
+                    mSummaryUpdateThread.interrupt();
+                    mSummaryUpdateThread = null;
+                }
+                EditText remove_die_nr = (EditText)findViewById(R.id.remove_die_nr);
+                EditText remove_die_sides = (EditText)findViewById(R.id.remove_die_sides);
+                try {
+                    int nr = Integer.parseInt(remove_die_nr.getText().toString());
+                    int sides = Integer.parseInt(remove_die_sides.getText().toString());
+                    if (nr > 0 && sides > 1) {
+                        Data.getInstance().removeDie(nr, sides);
+                    }
+                } catch (NumberFormatException e) {
+                    Log.i(TAG, "RemoveDie():NumberFormatException");
+                }
             }
         });
         Button bt_set_dice = (Button)findViewById(R.id.set_dice_button);
         bt_set_dice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (mRollUpdateThread != null) {
+                    mRollUpdateThread.interrupt();
+                    mRollUpdateThread = null;
+                }
+                if (mSummaryUpdateThread != null) {
+                    mSummaryUpdateThread.interrupt();
+                    mSummaryUpdateThread = null;
+                }
                 EditText set_dice_nr = (EditText)findViewById(R.id.set_dice_nr);
-                int nr = Integer.parseInt(set_dice_nr.getText().toString());
-                Data.getInstance().setDice(nr);
+                EditText set_dice_sides = (EditText)findViewById(R.id.set_dice_sides);
+                try {
+                    int nr = Integer.parseInt(set_dice_nr.getText().toString());
+                    int sides = Integer.parseInt(set_dice_sides.getText().toString());
+                    if (nr >= 0 && sides > 1) {
+                        Data.getInstance().setDice(nr, sides);
+                    }
+                } catch (NumberFormatException e) {
+                    Log.i(TAG, "SetDie():NumberFormatException");
+                }
             }
         });
         Button roll = (Button)findViewById(R.id.roll_die_button);
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Data.getInstance().roll();
-                TextView rolls = (TextView)findViewById(R.id.die_rolls);
+                if (mRollUpdateThread != null) {
+                    mRollUpdateThread.interrupt();
+                    mRollUpdateThread = null;
+                }
+                if (mSummaryUpdateThread != null) {
+                    mSummaryUpdateThread.interrupt();
+                    mSummaryUpdateThread = null;
+                }
+                TextView rolls = (TextView) ref.get().findViewById(R.id.die_rolls);
                 TextView total_result = (TextView)findViewById(R.id.total_result);
-                String die_rolls = "";
-                int total = Data.getInstance().getTotal();
-                for (int i = 0; i < Data.getInstance().getNrOfDie(); i++) {
-                    die_rolls += Data.getInstance().getDie(i) + ",";
-                }
-                if (!die_rolls.equals("")) {
-                    die_rolls = die_rolls.substring(0, die_rolls.length() - 1);
-                }
-                total_result.setText("" + total);
-                rolls.setText("(" + die_rolls + ")");
-                resultSummary();
+                total_result.setText("Calculating...");
+                rolls.setText("Rolling...");
+                Data.getInstance().roll();
             }
         });
 
         Data.getInstance().addObserver(this);
     }
 
-    private void resultSummary() {
-        int[] dice = new int[6];
-        for (int i = 0; i < Data.getInstance().getNrOfDie(); i++) {
-            switch (Data.getInstance().getDie(i)) {
-                case 1:
-                    dice[0]++;
-                    break;
-                case 2:
-                    dice[1]++;
-                    break;
-                case 3:
-                    dice[2]++;
-                    break;
-                case 4:
-                    dice[3]++;
-                    break;
-                case 5:
-                    dice[4]++;
-                    break;
-                case 6:
-                    dice[5]++;
-                    break;
-            }
+    static class RollUpdateUIHandler extends Handler {
+        WeakReference<MainActivity> ref;
+
+        public RollUpdateUIHandler(WeakReference<MainActivity> ref) {
+            this.ref = ref;
         }
 
-        TextView[] dice_rolls = new TextView[6];
+        @Override
+        public void handleMessage(Message msg) {
+            if (!Thread.currentThread().isInterrupted()) {
+                switch (msg.what) {
+                    case 0: // Interrupted
+                        Toast.makeText(ref.get().getApplicationContext(), "Roll interrupted...", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1: // All dice rolls finished
+                        TextView rolls = (TextView) ref.get().findViewById(R.id.die_rolls);
+                        rolls.setText("d" + Data.getInstance().getLargestDie() + "(" + msg.obj + ")");
+                        break;
+                    case 2: // Summary finished
+                        int[] dice = (int[])msg.obj;
+                        TableLayout tl = (TableLayout)ref.get().findViewById(R.id.dice_summary);
+                        tl.removeAllViews();
+                        // Three columns in table, calculate how rows we need
+                        int rows = (int)Math.ceil(Data.getInstance().getLargestDie() / 3.0f);
+                        TableRow[] tr = new TableRow[rows];
+                        for (int i = 0;i < rows; i++) {
+                            tr[i] = new TableRow(ref.get());
+                        }
 
-        dice_rolls[0] = (TextView)findViewById(R.id.dice_one_rolls);
-        dice_rolls[1] = (TextView)findViewById(R.id.dice_two_rolls);
-        dice_rolls[2] = (TextView)findViewById(R.id.dice_three_rolls);
-        dice_rolls[3] = (TextView)findViewById(R.id.dice_four_rolls);
-        dice_rolls[4] = (TextView)findViewById(R.id.dice_five_rolls);
-        dice_rolls[5] = (TextView)findViewById(R.id.dice_six_rolls);
+                        TextView[] textView = new TextView[Data.getInstance().getLargestDie()];
 
-        for (int i = 0; i < 6; i++) {
-            if (dice[i] > 0) {
-                dice_rolls[i].setTypeface(null, Typeface.BOLD);
-            } else {
-                dice_rolls[i].setTypeface(null, Typeface.NORMAL);
+                        int trCounter;
+                        for (int i = 0; i < textView.length; i++) {
+                            trCounter = i / 3;
+
+                            textView[i] = new TextView(ref.get());
+                            textView[i].setTextAppearance(android.R.style.TextAppearance_Material_Medium);
+
+                            if (dice[i] > 0) {
+                                textView[i].setTypeface(null, Typeface.BOLD);
+                            } else {
+                                textView[i].setTypeface(null, Typeface.NORMAL);
+                            }
+                            textView[i].setText((i+1) + "s: " + dice[i]);
+                            tr[trCounter].addView(textView[i]);
+                        }
+                        for (TableRow row: tr) {
+                            tl.addView(row);
+                        }
+
+                        break;
+                }
             }
-            dice_rolls[i].setText((i+1) + "s: " + dice[i]);
+            super.handleMessage(msg);
         }
+    }
 
+    private class SummaryUpdateUIThread extends Thread {
+        public void run() {
+            int[] dice = new int[Data.getInstance().getLargestDie()];
+            for (int i = 0; i < Data.getInstance().getNrOfDie(); i++) {
+                if(Thread.interrupted()) {
+                    Log.d(TAG, "SummaryUpdateUIThread(): interrupted");
+                    handler.sendEmptyMessage(0);
+                    return;
+                }
+                dice[Data.getInstance().getDie(i)-1]++;
+            }
+            handler.obtainMessage(2, dice).sendToTarget();
+            synchronized (MainActivity.this) {
+                mSummaryUpdateThread = null;
+            }
+        }
+    }
+
+    private class RollUpdateUIThread extends Thread {
+        public void run() {
+            String die_rolls;
+            StringBuilder stringBuilder = new StringBuilder(Data.getInstance().getNrOfDie()*2);
+            for (int i = 0; i < Data.getInstance().getNrOfDie(); i++) {
+                if(Thread.interrupted()) {
+                    Log.d(TAG, "RollUpdateUIThread(): interrupted");
+                    handler.sendEmptyMessage(0);
+                    return;
+                }
+                stringBuilder.append(Data.getInstance().getDie(i)).append(",");
+            }
+            if (stringBuilder.length() > 0) {
+                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            }
+            die_rolls = stringBuilder.toString();
+            handler.obtainMessage(1, die_rolls).sendToTarget();
+            synchronized (MainActivity.this) {
+                mRollUpdateThread = null;
+            }
+        }
+    }
+
+    private synchronized void diceRolled() {
+        if (mRollUpdateThread != null) {
+            mRollUpdateThread.interrupt();
+            mRollUpdateThread = null;
+        }
+        if (mSummaryUpdateThread != null) {
+            mSummaryUpdateThread.interrupt();
+            mSummaryUpdateThread = null;
+        }
+        mSummaryUpdateThread = new SummaryUpdateUIThread();
+        mSummaryUpdateThread.start();
+
+        mRollUpdateThread = new RollUpdateUIThread();
+        mRollUpdateThread.start();
+
+        int total = Data.getInstance().getTotal();
+        TextView total_result = (TextView)findViewById(R.id.total_result);
+        total_result.setText("" + total);
+    }
+
+    // TODO: Dices of different size
+    private void showDices() {
+        TextView dice = (TextView) findViewById(R.id.nrOfDice);
+        dice.setText("Dices: " + Data.getInstance().getNrOfDie() + "d" + Data.getInstance().getLargestDie());
     }
 
     @Override
     protected void onResume() {
+        showDices();
+        Data.getInstance().addObserver(this);
         super.onResume();
-        TextView dice = (TextView) findViewById(R.id.nrOfDice);
-        dice.setText("Dices: " + Data.getInstance().getNrOfDie());
+    }
+    @Override
+    protected void onPause() {
+        if (mRollUpdateThread != null) {
+            mRollUpdateThread.interrupt();
+            mRollUpdateThread = null;
+        }
+        if (mSummaryUpdateThread != null) {
+            mSummaryUpdateThread.interrupt();
+            mSummaryUpdateThread = null;
+        }
+        Data.getInstance().deleteObserver(this);
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        if (mRollUpdateThread != null) {
+            mRollUpdateThread.interrupt();
+            mRollUpdateThread = null;
+        }
+        if (mSummaryUpdateThread != null) {
+            mSummaryUpdateThread.interrupt();
+            mSummaryUpdateThread = null;
+        }
+        Data.getInstance().deleteObserver(this);
+        super.onStop();
     }
 
     @Override
     public void update(Observable o, Object obj) {
-        TextView dice = (TextView) findViewById(R.id.nrOfDice);
-        dice.setText("Dices: " + Data.getInstance().getNrOfDie() + "d6");
-
-
+        Log.d(TAG, "update");
+        if (Data.getInstance().getFlag(Data.FLAG_DICE_ROLL)) {
+            diceRolled();
+            Data.getInstance().setFlag(Data.FLAG_DICE_ROLL, false);
+        }
+        if (Data.getInstance().getFlag(Data.FLAG_DIEBAG_UPDATE)) {
+            showDices();
+            Data.getInstance().setFlag(Data.FLAG_DICE_ROLL, false);
+        }
     }
 }
